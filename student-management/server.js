184916478dc7422858d36bcf57fadccbe57832a1
cdn-bsct <justinarfeldman@gmail.com>
2023-01-30 10:07:37 -0500

    successful database connection

---
+++ student-management/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+// const path = require("path");
+const logger = require("morgan");
+
+require("dotenv").config();
+require("./config/database");
+
+const app = express();
+
+// app middlewares
+app.use(logger("dev"));
+app.use(express.json());
+
+// API routes below
+// Users routes
+// app.use("/api/students", require("./routes/students"));
+
+const port = process.env.PORT || 3001;
+
+app.listen(3001, function () {
+  console.log(`Express app running on port ${port}`);
+});
