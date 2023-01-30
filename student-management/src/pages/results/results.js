c999c1f881348d4ef5d259f57c5098a7bf38283c
cdn-bsct <justinarfeldman@gmail.com>
2023-01-30 18:30:12 -0500

    results page started

--- student-management/src/pages/results/results.js
+++ student-management/src/pages/results/results.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Results() {
   return (
     <>
-      <div>results page</div>
+      <h1>Results Page</h1>
+      <hr />
+      <form>
+        <select type="select" name="student" placeholder="Student"></select>
+        <select type="select" name="course" placeholder="Course"></select>
+        <select type="select" name="result" placeholder="Result">
+          <option>A</option>
+          <option>B</option>
+          <option>C</option>
+          <option>D</option>
+          <option>E</option>
+          <option>F</option>
+        </select>
+        <input type="submit" value="Submit" />
+      </form>
     </>
   );
 }
