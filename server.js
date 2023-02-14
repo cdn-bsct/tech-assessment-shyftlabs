ddf13747e0de75b837bbbd66354c74badec0c8af
cdn-bsct <justinarfeldman@gmail.com>
2023-02-14 16:23:18 -0500

    Heroku error address - h10

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
-// const path = require("path");
+const path = require("path");
+const favicon = require("serve-favicon");
 const logger = require("morgan");
 
 require("dotenv").config();
 require("./config/database");
 
 const app = express();
+
+app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
+app.use(express.static(path.join(__dirname, "build")));
 
 // app middlewares
 app.use(logger("dev"));
@@ -16,7 +20,7 @@ app.use("/api/students", require("./routes/students"));
 app.use("/api/courses", require("./routes/courses"));
 app.use("/api/results", require("./routes/results"));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(3001, function () {
   console.log(`Express app running on port ${port}`);
